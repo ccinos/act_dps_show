90ba066ef6d5d6bc92e3704c289d5c867d68454f
ccinos <ccinos@163.com>
2019-11-29 08:57:41 +0800

    Update common.js

--- common.js
+++ common.js
@@ -1,7 +1,11 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?c697c926ed88ef936e8708d1a13a6b0a";
+  if(location.host.indexOf("gitee")!=-1){
+    hm.src = "https://hm.baidu.com/hm.js?9243edb854e159d4be5cd7fcba1ba56c";
+  }else{
+    hm.src = "https://hm.baidu.com/hm.js?c697c926ed88ef936e8708d1a13a6b0a";
+  }
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
