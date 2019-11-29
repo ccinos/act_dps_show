9c653a27129120fbe166794f8d5d39790f07fbcc
ccinos <ccinos@163.com>
2019-11-29 08:20:50 +0800

    增加统计

---
+++ common.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c697c926ed88ef936e8708d1a13a6b0a";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
