174d291cdac16167999e117ae602a9b85feed72b
ccinos <ccinos@163.com>
2020-01-08 17:42:25 +0800

    add 时间轴

--- common.js
+++ common.js
@@ -1,34 +1,55 @@
 var _hmt = _hmt || [];
-(function() {
+(function () {
   var hm = document.createElement("script");
-  if(location.host.indexOf("gitee")!=-1){
+  if (location.host.indexOf("gitee") != -1) {
     hm.src = "https://hm.baidu.com/hm.js?9243edb854e159d4be5cd7fcba1ba56c";
-  }else{
+  } else {
     hm.src = "https://hm.baidu.com/hm.js?c697c926ed88ef936e8708d1a13a6b0a";
   }
-  var s = document.getElementsByTagName("script")[0]; 
+  var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
-function padLeft(s,c,length){
-  while(s.length<length){
-      s=c+s
+function padLeft(s, c, length) {
+  while (s.length < length) {
+    s = c + s
   }
   return s;
 }
-function mergeObj(dest,src){
-  var type=Object.prototype.toString.call(src);
-  if(type=="[object Object]"||type=="[object Array]"){
-      for(var i in src){
-          if(src.hasOwnProperty(i)){
-              type=Object.prototype.toString.call(src[i]);
-              if(type=="[object Object]"){
-                  dest[i]=mergeObj(dest[i]||{},src[i]);
-              }else{
-                  dest[i]=src[i];
-              }
-          }
+function mergeObj(dest, src) {
+  var type = Object.prototype.toString.call(src);
+  if (type == "[object Object]" || type == "[object Array]") {
+    for (var i in src) {
+      if (src.hasOwnProperty(i)) {
+        type = Object.prototype.toString.call(src[i]);
+        if (type == "[object Object]") {
+          dest[i] = mergeObj(dest[i] || {}, src[i]);
+        } else {
+          dest[i] = src[i];
+        }
       }
-      
+    }
+
   }
   return dest;
+}
+var formatDate = function (date, fmt) {
+  fmt = fmt || "yyyy-MM-dd hh:mm:ss";
+  var o = {
+    "y+": date.getFullYear(),
+    "M+": date.getMonth() + 1, //月份
+    "d+": date.getDate(), //日
+    "h+": date.getHours(), //小时
+    "m+": date.getMinutes(), //分
+    "s+": date.getSeconds(), //秒
+    "f+": date.getMilliseconds(), //毫秒
+  };
+
+  for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt))
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1)
+        ? (o[k]) : (("0000" + o[k]).substr(("" + o[k]).length + 4 - RegExp.$1.length)));
+  return fmt;
+}
+Date.prototype.format=function(fmt){
+  return formatDate(this,fmt);
 }
---
+++ simple_timeline.js
@@ -0,0 +1,23 @@
+
+
+//时间轴
+var actions=[
+    {
+        name:"铁壁", //名称
+        cd:90, //冷却 秒
+        duration:20, //持续
+        relief:0.8, //减伤系数
+        shield:15000, //盾值
+    }
+]
+
+
+var timeline=[ //时间轴
+    {
+        time:12345, //时间，毫秒
+        type:"event",
+        title:"大AOE", //标题
+        damage:50000,
+        desc:"造成百分比伤害，需要开小减伤", //描述
+    }
+];
